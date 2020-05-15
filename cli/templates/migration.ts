068e89b3ea1920a44d11cb34e829e13a9d46abc3
halvardssm <halvard@simplyundoable.com>
2020-05-15 16:10:39 +0200

    fixed typo

--- cli/templates/migration.ts
+++ cli/templates/migration.ts
@@ -1,6 +1,6 @@
 import { Schema } from "https://deno.land/x/nessie/mod.ts";
 
-export const up = (scema: Schema): void => {
+export const up = (schema: Schema): void => {
 };
 
 export const down = (schema: Schema): void => {
