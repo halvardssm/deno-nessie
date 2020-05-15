98ae50229ab4d531f5f555d8da6948cada5a2441
halvardssm <halvard@simplyundoable.com>
2020-05-15 12:29:12 +0200

    relative p2

--- cli/state.ts
+++ cli/state.ts
@@ -122,6 +122,6 @@ export class State {
       return path;
     }
 
-    return relative(Deno.cwd(), path ?? defaultFolder ?? "");
+    return "./" + relative(Deno.cwd(), path ?? defaultFolder ?? "");
   }
 }
