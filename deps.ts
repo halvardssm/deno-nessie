56a6f4542bba8f11ec0692fb5eaf18a4a73da756
halvardssm <halvard@simplyundoable.com>
2020-10-07 20:48:22 +0200

    updated versions

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-export { relative, resolve } from "https://deno.land/std@0.70.0/path/mod.ts";
+export { relative, resolve } from "https://deno.land/std@0.73.0/path/mod.ts";
 export {
   assert,
   assertArrayContains,
   assertEquals,
-} from "https://deno.land/std@0.70.0/testing/asserts.ts";
+} from "https://deno.land/std@0.73.0/testing/asserts.ts";
 
 import Denomander from "https://deno.land/x/denomander@0.6.3/mod.ts";
 
