f2e97b7d69cd0f7b6aefcca375931bb168b199f6
Aphagon Phromdesarn <aphagon@gmail.com>
2020-12-28 08:20:14 +0700

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -6,6 +6,6 @@ export {
 } from "https://deno.land/std@0.82.0/testing/asserts.ts";
 
 // Denomander v.0.7.01
-import Denomander from "https://raw.githubusercontent.com/siokas/denomander/f0ce9c1c0a758a5df4e886e03277388c74658859/mod.ts";
+import Denomander from "https://denopkg.com/siokas/denomander@0.7.01/mod.ts";
 
 export { Denomander };
