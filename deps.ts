e96cf042a6cb0639075fec7dbabb6c534e17fd28
Halvard Mørstad <halvard@simplyundoable.com>
2021-01-10 18:55:28 +0100

    added version to denomander

--- deps.ts
+++ deps.ts
@@ -5,6 +5,6 @@ export {
   assertEquals,
 } from "https://deno.land/std@0.83.0/testing/asserts.ts";
 
-import Denomander from "https://raw.githubusercontent.com/siokas/denomander/master/mod.ts";
+import Denomander from "https://raw.githubusercontent.com/siokas/denomander/0.7.01/mod.ts";
 
 export { Denomander };
