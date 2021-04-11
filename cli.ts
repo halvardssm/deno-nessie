491af27bd2483519e199dfd4e9cddf25f4a3ede7
Halvard Mørstad <halvard@simplyundoable.com>
2021-04-11 12:30:50 +0200

    fixed import

--- cli.ts
+++ cli.ts
@@ -2,11 +2,11 @@ import { State } from "./cli/state.ts";
 import { Denomander, format, resolve } from "./deps.ts";
 import {
   REGEX_MIGRATION_FILE_NAME_LEGACY,
+  URL_BASE,
   URL_TEMPLATE_BASE,
   URL_TEMPLATE_BASE_VERSIONED,
   VERSION,
 } from "./consts.ts";
-import { URL_BASE } from "./consts";
 
 /** Initializes Denomander */
 const initDenomander = () => {
