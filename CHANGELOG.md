fe51cdf61bf7e066658a484253359a22404a89b4
Halvard Mørstad <halvard@simplyundoable.com>
2021-05-17 14:16:17 +0200

    bump 1.3.2:1.10.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## Version 1.3.2
 
+- Deno v1.10.1
 - Fixed bug where path for async import was not a file url
 
 ## Version 1.3.1
--- cli/utils.ts
+++ cli/utils.ts
@@ -1,9 +1,9 @@
 import { resolve } from "../deps.ts";
 
-/** Helper function for path parsing. 
- * 
- * If the first element starts with `http` or `https` or `file` it will return the first element. 
- * Else, the path will be prefixed by `file://` and resolved. 
+/** Helper function for path parsing.
+ *
+ * If the first element starts with `http` or `https` or `file` it will return the first element.
+ * Else, the path will be prefixed by `file://` and resolved.
  */
 export const parsePath = (...path: string[]): string => {
   if (path.length === 1 && isUrl(path[0])) {
