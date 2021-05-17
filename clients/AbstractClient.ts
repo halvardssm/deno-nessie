06893dbc243e12fe43657a7b698a8c3504650238
Halvard Mørstad <halvard@simplyundoable.com>
2021-05-17 14:03:27 +0200

    readded file url to path string

--- clients/AbstractClient.ts
+++ clients/AbstractClient.ts
@@ -25,6 +25,7 @@ import {
   REGEX_MIGRATION_FILE_NAME_LEGACY,
   TABLE_MIGRATIONS,
 } from "../consts.ts";
+import { parsePath } from "../cli/utils.ts";
 
 /** The abstract client which handles most of the logic related to database communication. */
 export abstract class AbstractClient<Client> {
@@ -274,7 +275,7 @@ export abstract class AbstractClient<Client> {
     }
 
     options.migrationFolders?.forEach((folder) => {
-      this.migrationFolders.push(resolve(Deno.cwd(),folder));
+      this.migrationFolders.push(resolve(Deno.cwd(), folder));
     });
 
     if (this.migrationFolders.length < 1) {
@@ -291,7 +292,7 @@ export abstract class AbstractClient<Client> {
     }
 
     options.seedFolders?.forEach((folder) => {
-      this.seedFolders.push(resolve(Deno.cwd(),folder));
+      this.seedFolders.push(resolve(Deno.cwd(), folder));
     });
 
     if (this.seedFolders.length < 1) {
@@ -315,7 +316,7 @@ export abstract class AbstractClient<Client> {
         .filter((file) => file.isFile && this._isMigrationFile(file.name))
         .map((file) => ({
           name: file.name,
-          path: resolve(folder, file.name),
+          path: parsePath(folder, file.name),
         }));
 
       this.migrationFiles.push(...filesRaw);
@@ -334,7 +335,7 @@ export abstract class AbstractClient<Client> {
         .filter((file) => file.isFile)
         .map((file) => ({
           name: file.name,
-          path: resolve(folder, file.name),
+          path: parsePath(folder, file.name),
         }));
 
       this.seedFiles.push(...filesRaw);
