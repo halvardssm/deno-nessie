e9b00a3bb39a388b42299cde9680e5cd6bdfb759
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2022-01-04 13:31:54 +0100

    adding MySQLClient55

--- tests/unit/templates.test.ts
+++ tests/unit/templates.test.ts
@@ -9,6 +9,7 @@ import { DB_DIALECTS, URL_BASE_VERSIONED } from "../../consts.ts";
 Deno.test("getConfigTemplate standard", () => {
   const expected = `import {
     ClientMySQL,
+    ClientMySQL55,
     ClientPostgreSQL,
     ClientSQLite,
     NessieConfig,
@@ -24,6 +25,14 @@ Deno.test("getConfigTemplate standard", () => {
 // });
 
 // const client = new ClientMySQL({
+//     hostname: "localhost",
+//     port: 3306,
+//     username: "root",
+//     // password: "pwd", // uncomment this line for <8
+//     db: "nessie",
+// });
+
+// const client = new ClientMySQL55({
 //     hostname: "localhost",
 //     port: 3306,
 //     username: "root",
